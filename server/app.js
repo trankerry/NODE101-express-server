539b0f4dc131ba8972f361e294c332be923aabf8
Kerry <trankerry94@gmail.com>
2021-11-09 15:06:04 -0800

    finished project

--- server/app.js
+++ server/app.js
@@ -1,15 +1,22 @@
 // import files and packages up here
+const topSpots = require('./data.json');
 const express = require('express');
 const morgan = require('morgan');
 
-
+const PORT = process.env.PORT || 3000;
 // create your express server below
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log("It's working"));
 // add your routes and middleware below
-app.get('/')
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+    res.status(200).send('New Express Server');
+});
+
+app.get('/data', function(req,res) {
+    res.status(200).send(topSpots)
+})
 
 // finally export the express application
 module.exports = app;
