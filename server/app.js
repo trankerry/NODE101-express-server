642e98a763af4b171855160cc7db06121958ede9
Kerry <trankerry94@gmail.com>
2021-11-08 16:27:10 -0800

    working on express server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
 
 
 // create your express server below
-var app;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => console.log("It's working"));
 // add your routes and middleware below
+app.get('/')
 
 // finally export the express application
 module.exports = app;
